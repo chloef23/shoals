e948e4bd971ce5f5ca3775456e04da9995648390
Chloe Fugle <chloe.m.fugle.23@dartmouth.edu>
2022-06-18 10:51:01 -0400

    add data sort by species, credits

--- fledged_present.R
+++ fledged_present.R
@@ -1,3 +1,20 @@
+# Chloe Fugle (chloe.m.fugle.23@dartmouth.edu), 6/18/22
+# Shoals Marine Laboratory internship
+# purpose:
+# all data from Gulls of Appledore Database, primary contact:
+#          Mary Elizabeth Everett (MElizabethEverett@gmail.com)
+
+# import packages
 library(readxl)
-number_fledged_present <- read_excel("~/2021-2022/Shoals/Data/failure_reason.xlsx")
-View(number_fledged_present)
+library(dplyr)
+
+# read data from excel spreadsheet into dataframe
+# excel spreadsheet contains gull data sorted for
+#       field readable tag (male or female), number fledged,
+#       latitude, and longitude is not null
+df <- read_excel("~/2021-2022/Shoals/Data/field_readable.xlsx")
+View(df)
+
+# sort dataframe by species
+herg = df %>% filter(grepl('HERG', species))
+gbbg = df %>% filter(grepl('GBBG', species))
