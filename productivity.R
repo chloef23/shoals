3eb4b8538ba589bfbb15544ac11dcdf5c0cf17e9
Chloe Fugle <chloe.m.fugle.23@dartmouth.edu>
2022-07-03 19:26:43 -0400

    Remove uncertain neighborhoods

--- productivity.R
+++ productivity.R
@@ -8,6 +8,7 @@ PRODUCTIVITY_DATA = "C:/Users/sapph/Downloads/ROST productivity raw data 2016-20
 library(readxl)
 library(dplyr)
 
+################################################################################
 # import data from "ROST productivity raw data 2016-2021.xlsx"
 prod_2017 <- read_excel(PRODUCTIVITY_DATA, sheet="Productivity 2017")
 prod_2018 <- read_excel(PRODUCTIVITY_DATA, sheet="Productivity 2018")
@@ -15,7 +16,7 @@ prod_2019 <- read_excel(PRODUCTIVITY_DATA, sheet="Productivity 2019")
 prod_2020 <- read_excel(PRODUCTIVITY_DATA, sheet="Productivity 2020")
 prod_2021 <- read_excel(PRODUCTIVITY_DATA, sheet="Productivity 2021")
 
-# get neighborhood and fledged/dead data
+# extract neighborhood and fledged/dead data from raw data
 # note: code is specific to each year because the data is recorded differently
 fledged_2017 = prod_2017[c("Plot/Area...1", "Notes", "Final Disposition")]
 fledged_2017['Year'] = 2017
@@ -49,5 +50,61 @@ fledged_2021['Notes'] = NA
 fledged_2021['Fledged'] = prod_2021[,23]
 colnames(fledged_2021) = c("Year", "Neighborhood", "Notes", "Fledged")
 
-# combine year dataframes into one
+########################################################################
+# combine year dataframes into one and process the data
 fledged_all <- rbind(fledged_2017, fledged_2018, fledged_2019, fledged_2020, fledged_2021)
+
+# function to extract numbers from string
+# input: x - string
+# output: x_numbers - list of the single-digit integers contained in the string
+extract_nums = function(x, output){
+  x_numbers <- regmatches(x, gregexpr("[[:digit:]]+", x))
+  return(x_numbers)
+}
+
+# function to collapse list of integers into string
+# input: x - list of numbers
+# output: x_string - string of numbers
+collapse_nums = function(x, output){
+  x_string = paste(unlist(x),collapse="")
+  return(x_string)
+}
+
+# extract neighborhood number from string
+neigh_num <- apply(fledged_all[,2], 1, extract_nums)
+neigh_num_str <- lapply(neigh_num, collapse_nums)
+fledged_all$Neighborhood_Number <- neigh_num_str
+
+# remove rows with uncertain neighborhood (have multiple neighborhoods listed)
+fledged_all_seg <- subset(fledged_all, nchar(Neighborhood_Number) == 1)
+
+# remove any rows where Notes say "exclude from productivity"
+fledged_all_seg <- subset(fledged_all_seg, Notes != "exclude from productivity"
+                          | is.na(Notes))
+
+# function that returns 0 if the string matches "dead" or "abandoned", NA if
+#   string matches "missing", and 1 if string matches "fledged" and the first two
+#   conditions aren't met
+# input: x - string
+# output: x_num - value is 0, 1, or NA
+check_alive = function(x, output){
+  x_num = NA
+  if(is.na(x) | x == ""){
+    return(NA)
+  }
+  
+  x <- tolower(x)
+  if(grepl("dead", x) | grepl("abandonded", x)){
+    x_num = 0
+  }
+  else if(grepl("missing", x)){
+    x_num = NA
+  }
+  else if(grepl("fledged", x)){
+    x_num = 1
+  }
+}
+
+# assign 1 for fledged chicks, 0 for dead chicks or abandoned eggs, and NA
+# for missing chicks
+alive <- apply(fledged_all_seg[,4], 1, check_alive())
